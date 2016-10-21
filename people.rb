5bf2d8fcea90b6390e6b5113d79b5c9e2cb9a338
LUKE PLOURDE <lukeplourde@hotmail.com>
2016-10-21 15:05:29 -0400

    Student is 'learnding' and teacher is teaching added

---
+++ people.rb
@@ -0,0 +1,19 @@
+# Create class called Student.
+class Student
+
+#Made unique greeting in method
+  def learn
+    puts "I'm learnding!"
+  end
+
+end
+
+#Create class called Instructor
+class Instructor
+
+#Made unique greeting in method
+  def teach
+  puts "Everything in ruby is an object"
+  end
+
+end
