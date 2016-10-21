8b17dcbdc1efc6bde392f2e11af8cc3ae14bdb89
LUKE PLOURDE <lukeplourde@hotmail.com>
2016-10-21 19:39:22 -0400

    Added Receipt return

--- sales_tax.rb
+++ sales_tax.rb
@@ -1,12 +1,62 @@
 class Item
 
+  # attr_writer :chocolate :perfume :pills
+
+
   def initialize(quantity, name, price)
     @quantity = quantity
     @name     = name
     @price    = price
   end
-  
+
+  def own
+    ("#{@quantity} #{@name} at #{@price}")
+  end
+
+  def basic
+    ("#{@price}").to_f * 0.10 + ("#{@price}").to_f
+        end
+
+  def import
+    ("#{@price}").to_f * 0.05 + ("#{@price}").to_f
+  end
+#Example one
+book = Item.new(1,"Book", 12.49)
+puts book.own
+music_cd = Item.new(1,"Music CD", 14.99)
+puts music_cd.own
+chocolate_bar = Item.new(1,"Chocolate bar", 0.85)
+puts chocolate_bar.own
+
+
 end
 
-class Receipt
+class Receipt < Item
+
+  def own
+    ("#{@quantity} #{@name}: #{@price}")
+  end
+
+  def basic
+    ("#{@price}").to_f * 0.10 + ("#{@price}").to_f
+  end
+
+        def import
+          ("#{@price}").to_f * 0.05 + ("#{@price}").to_f
+        end
+
+        puts "-------------------------"
+
+        def tally(adding)
+          adding.each do |x|
+            x + ("#{@price}")
+          end
+        end
 end
+
+book = Receipt.new(1,"Book", 12.49)
+puts book.own
+music_cd = Receipt.new(1,"Music CD", 14.99)
+puts music_cd.basic
+chocolate_bar = Receipt.new(1,"Chocolate bar", 0.85)
+puts chocolate_bar.own
